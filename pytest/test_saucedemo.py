bf5dd5e37a52070015984538f36e0b385cf6ba1f
Ian Hill <ian.g.hill@outlook.com>
2024-06-18 15:20:55 +0100

    skip and only browser

--- pytest/test_saucedemo.py
+++ pytest/test_saucedemo.py
@@ -2,6 +2,21 @@ from playwright.sync_api import Page
 import pytest
 
 
-def test_title(page: Page):
-    page.goto("http://saucedemo.com")
+@pytest.mark.skip_browser("chromium")
+def test_skip_title(page: Page):
+    page.goto("https://saucedemo.com")
     assert page.title() == "Swag Labs"
+
+
+@pytest.mark.only_browser("chromium")
+def test_title(page: Page):
+    page.goto("https://saucedemo.com")
+    assert page.title() == "Swag Labs"
+
+
+def test_inventory_not_logged_in(page: Page):
+    page.goto("https://saucedemo.com/inventory.html")
+    assert (
+        page.inner_text("h3")
+        == "Epic sadface: You can only access '/inventory.html' when you are logged in."
+    )
